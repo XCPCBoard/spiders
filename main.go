a5714fb26858a44ad61115c5edb9e4d9604dabeb
AcidMangos <1124497942@qq.com>
2022-07-06 13:36:40 +0800

    update atcoder

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	_ "XCPCer_board/config"
 	"XCPCer_board/dao"
 	_ "XCPCer_board/dao"
+	"XCPCer_board/model"
+	"XCPCer_board/spider/atcoder"
 	_ "github.com/FengZhg/go_tools/gin_logrus"
 )
 
 // 主入口函数
 func main() {
-	
+	atcoder.Flush(model.TestAtcIdLQY)
 }
 
 func init() {
