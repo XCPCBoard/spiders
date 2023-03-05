5e4abf83277fc8d0a111b6b9d7110d9a7344a56b
Crescent12138 <75103155+Crescent12138@users.noreply.github.com>
2023-03-05 23:41:19 +0800

    增加定时器，对数据库进行访问

--- main.go
+++ main.go
@@ -4,11 +4,45 @@ import (
 	_ "XCPCer_board/config"
 	"XCPCer_board/dao"
 	_ "XCPCer_board/dao"
+	"XCPCer_board/spider/codeforces"
+	"XCPCer_board/spider/nowcoder"
+	log "github.com/sirupsen/logrus"
+	//"XCPCer_board/spider/nowcoder"
 	_ "github.com/FengZhg/go_tools/gin_logrus"
 )
 
 // 主入口函数
 func main() {
+	c := cron.New()
+	c.AddFunc("@every 24s", func() {
+		log.Infoln("okk")
+		ls, err := dao.DBClient.Query("select uid,platform from id_platform;")
+		defer ls.Close()
+		if err != nil {
+			log.Errorf("database error")
+			return
+		}
+		for ls.Next() {
+			id, platform := "", ""
+			err := ls.Scan(&id, &platform)
+			if err != nil {
+				log.Errorln(err)
+				return
+			}
+			if platform == "codeforces" {
+				codeforces.Flush(id)
+			} else if platform == "nowcoder" {
+				nowcoder.Flush(id)
+			}
+
+		}
+	})
+	c.Start()
+	go c.Start()
+	defer c.Stop()
+
+	select {}
+
 }
 
 func init() {
